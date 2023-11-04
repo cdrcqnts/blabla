4faac1effb1c25d2c82a75a35ece1a9713a80851
cdrcqnts <cdrcqnts@gmail.com>
2023-11-04 14:00:57 +0100

    bla

--- backend/app/test_main.py
+++ backend/app/test_main.py
@@ -7,7 +7,7 @@ client = TestClient(app)
 
 def test_root():
     response = client.get("/")
-    assert response.status_code != 200
+    assert response.status_code == 200
 
 
 def test_read_item():
